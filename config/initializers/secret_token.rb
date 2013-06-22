4c0414148f8b0779571f1597fdf2a47a9c4d7622
cameel <cameel2@gmail.com>
2013-06-22 02:28:38 +0200

    rails new fabtastic

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fabtastic::Application.config.secret_token = 'a7e54e65624f3135ce37c4de417a6db92a203e1dec48a9c6cff1949be0c3132a045e978224c4ed5ed3340642c3c3d63b8e4f9889cdaae9b68332e036eeab360b'
